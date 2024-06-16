0a2ba638ebcf28ed0311f89ac5ac30c65502e288
Kaue Bosel <kauebosel@gmail.com>
2024-06-16 09:58:20 -0300

    Colocando exercício finalizado TwoSum (1.) no git

--- AED1/1.c
+++ AED1/1.c
@@ -1,33 +1,24 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 
-typedef struct celula {
-    char conteudo;
-    struct celula *prox;
-}cel;
-
-int main(){
-    int menu=0, tamanhoNome=0, *pt;
-    char str[200];
-
-
-    do{
-        printf("1.adiciona nome, 2.remove nome, 3.lista, 4.sair\n");
-        scanf("%d",&menu);
-
-        if(menu == 1){
-            printf("digite o nome (com caractere '_' para separar)\n");
-            getchar();
-            fgets(str, sizeof(str), stdin);
-            *pt = strlen(str);
-            printf("%d", *pt);
-
-            /*FILE *fp = fopen("arquivo.txt", "w+");
-            if(fp == NULL){
-                printf("problema 1\n");
-                return 1;
-            }*/
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    int *returnArr = (int*)malloc(sizeof(int) * 2);
+    returnArr[0] = 0;
+    returnArr[1] = 0;
+    
+    for(int i = 0; i < numsSize; i++){
+        for(int j = 0; j < numsSize; j++){
+            if(nums[i] + nums[j] == target && i != j){
+                *returnSize = 2;
+                returnArr[0] = i;
+                returnArr[1] = j;
+                return returnArr;
+            }
         }
-    } while(menu != 4);
+    }
+    *returnSize = 0;
+    return NULL;
 }
