1fed920ceabd01b2131c0482d72ee7d75e007f31
Kaue Bosel <kauebosel@gmail.com>
2024-07-05 10:56:16 -0300

    sincronização python e VSCode

---
+++ Python/1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        i = 0
+        j = 0
+        arr = [a,b]
+        for n in nums:
+            if nums[i] + nums[j] == target:
+            arr[a] = i
+            arr[b] = j
+            return arr
